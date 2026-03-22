066f3ba0a28f8b2d2df05454f2edbb12749b814c
Ryan Faulhaber <ryf@sent.as>
2026-03-22 11:35:57 -0400

    version bump

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
             pkgs.rustPlatform.buildRustPackage {
               inherit features;
               pname = projectName;
-              version = "0.1.0";
+              version = "0.2.0";
               src = ./.;
               cargoLock.lockFile = ./Cargo.lock;
             };
